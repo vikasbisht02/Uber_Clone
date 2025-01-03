b8f5a6162856d4b55047922522dc4707309cbf03
Vikas Bisht <vikasbisht732002@gmail.com>
2025-01-02 19:58:47 -0500

    In frontend User and Captain Home page and other components completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,9 +12,9 @@ import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
 import CaptainLogout from './pages/CaptainLogout'
-//import Riding from './pages/Riding'
-//import CaptainRiding from './pages/CaptainRiding'
-//import 'remixicon/fonts/remixicon.css'
+import Riding from './pages/Riding'
+import CaptainRiding from './pages/CaptainRiding'
+import 'remixicon/fonts/remixicon.css'
 
 const App = () => {
 
@@ -23,9 +23,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Start />} />
         <Route path='/login' element={<UserLogin />} />
-        {/* <Route path='/riding' element={<Riding />} />
-        <Route path='/captain-riding' element={<CaptainRiding />} /> */}
-
+        <Route path='/riding' element={<Riding />} />
+        <Route path='/captain-riding' element={<CaptainRiding />} />
         <Route path='/signup' element={<UserSignup />} />
         <Route path='/captain-login' element={<Captainlogin />} />
         <Route path='/captain-signup' element={<CaptainSignup />} />
